54ee71c1b2902ca5d44087621dd8d9e69a8a5265
Administrator <Administrator@windows10.microdone.cn>
2018-01-13 15:12:28 +0800

    增加日志

--- src/main/java/com/chen/hdfsspring/controller/MapReduceController.java
+++ src/main/java/com/chen/hdfsspring/controller/MapReduceController.java
@@ -1,5 +1,7 @@
 package com.chen.hdfsspring.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -11,11 +13,15 @@ import com.chen.hdfsspring.service.mapreduce.WordCount;
 @Controller
 @RequestMapping(value="mapreduce")
 public class MapReduceController {
-	
+	Logger logger = LoggerFactory.getLogger(MapReduceController.class);
 	@RequestMapping(value="/wordcount.action",method=RequestMethod.GET, produces="application/json;charset=UTF-8")
 	public @ResponseBody String generateResult(@RequestParam(defaultValue="")String input, @RequestParam(defaultValue="")String output) throws Exception{
-		WordCount v = WordCount.getInstance();
-		v.doJob(input, output);
+		try {
+			WordCount v = WordCount.getInstance();
+			v.doJob(input, output);
+		} catch (Exception e) {
+			logger.error(e.getMessage(), e);
+		}
 		return "success";
 	}
 }
